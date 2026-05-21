5340e7c4a53599e7e112cdb9c613c3e30b96e145
Yion <6831503029@lamduan.mfu.ac.th>
2026-05-21 11:57:31 +0700

    Fix: baseroute fix

--- frontend/lib/constants.tsx
+++ frontend/lib/constants.tsx
@@ -1,5 +1,5 @@
-// export const baseUrl = "https://yiondev.me";
-export const baseUrl = "http://localhost:3000";
+export const baseUrl = "https://yiondev.me";
+// export const baseUrl = "http://localhost:3000";
 export const blogUrl = "/blogs"
 export const projectUrl = "/projects"
 
