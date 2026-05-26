6270efcc391482a37aa76d81382bfcd3f39072f5
Yion <6831503029@lamduan.mfu.ac.th>
2026-05-26 10:31:26 +0700

    Fix: CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -14,7 +14,12 @@ Base.metadata.create_all(bind=engine);
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://www.yiondev.me"],
+    allow_origins=[
+        "https://www.yiondev.me",
+        "https://yiondev.me",
+        "http://localhost:3000"
+    ],
+    allow_cerdentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
