f4aa3c99b24d58bf58d1e5b8628cda42d8f078b0
Yion <6831503029@lamduan.mfu.ac.th>
2026-07-27 09:19:54 +0700

    Fix: Nginx

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,13 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-      remotePatterns: [
-          {
-              protocol: "https",
-              hostname: "lruzdrf7t7zl7ff6.public.blob.vercel-storage.com"
-          }
-      ]
-    }
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lruzdrf7t7zl7ff6.public.blob.vercel-storage.com",
+      },
+    ],
+  },
+  generateBuildId: async () => {
+    return process.env.GIT_SHA || "yion-build";
+  },
 };
 
 export default nextConfig;
